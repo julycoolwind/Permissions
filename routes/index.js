ddc9bbab5b7c6755ec2f4a8a5fc68d9e63729ca7
ZhangRGK <stamen0630@gmail.com>
2014-02-07 14:05:13 +0800

    modify banner

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,56 @@
-
 /*
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: '用户系统范例',logo:'MainPage','menus':['按钮1','按钮2','按钮3','按钮4','5','6','7','8','9'],'navis':['navi1','navi2','navi3','navi4']});
+var menus = [
+    {
+        'name': '按钮1',
+        'children': [
+            {
+                'name': 'nav1',
+                'url': 'url1'
+            },
+            {
+                'name': 'nav2',
+                'url': 'url2'
+            },
+            {
+                'name': 'nav3',
+                'url': 'url3'
+            },
+            {
+                'name': 'nav4',
+                'url': 'url4'
+            },
+            {
+                'name': 'nav5',
+                'url': 'url5'
+            }
+        ]
+    },
+    {
+        'name': '按钮2',
+        'url': 'url2'
+    },
+    {
+        'name': '按钮3',
+        'children': [
+            {
+                'name': 'nav1',
+                'url': 'url1'
+            },
+            {
+                'name': 'nav2',
+                'url': 'url2'
+            },
+            {
+                'name': 'nav3',
+                'url': 'url3'
+            }
+        ]
+    },
+]
+
+exports.index = function (req, res) {
+    res.render('index', { title: '用户系统范例', logo: 'MainPage', 'menus': menus});
 };
