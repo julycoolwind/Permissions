562f1c5e1e8e21218cc14db1cee0801d3342c4fc
ZhangRGK <stamen0630@gmail.com>
2014-02-11 11:39:39 +0800

    log4js

---
+++ util/logger.js
@@ -0,0 +1,23 @@
+var log4js = require('log4js');
+
+//log4js config
+log4js.configure({
+    "appenders": [
+        {
+            "type": "console"
+        },
+        {
+            "type": "dateFile",
+            "filename": "log/permissions.log",
+            "pattern": "-yyyy-MM-dd",
+            "alwaysIncludePattern": false
+        }
+    ],
+    "replaceConsole": true
+});
+
+exports.logger = function(moduleName) {
+    var logger = log4js.getLogger(moduleName);
+    logger.setLevel("DEBUG");
+    return logger;
+};
